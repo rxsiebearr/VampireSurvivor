5504c7d72027981764d3cc7ca63ca48468d7a210
ylirin <xili1@s.sfusd.edu>
2024-05-08 10:52:54 -0700

    Camera and Weapon and Aim

--- mob.gd
+++ mob.gd
@@ -4,6 +4,6 @@ extends CharacterBody2D
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * 100.0
+	velocity = direction * 800.0
 	move_and_slide()
 	
