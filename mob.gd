7378cb1264778e2be0390bc3f13238b711267907
rxsiebearr <chzou1@s.sfusd.edu>
2024-05-10 15:45:50 -0700

    Fixed slime take damage

--- mob.gd
+++ mob.gd
@@ -1,9 +1,20 @@
 extends CharacterBody2D
 
+var health = 3
 @onready var player = get_node("/root/Game/Player")
+
+func _ready():
+	%Slime.play_walk()
+	
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * 800.0
+	velocity = direction * 300.0
 	move_and_slide()
+
+func take_damage():
+	health -= 1
+	%Slime.play_hurt()
 	
+	if health == 0:
+		queue_free()
--- player.gd
+++ player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 func _physics_process(delta:):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = direction * 2000
+	velocity = direction * 600.0
 	move_and_slide()
 
 	if velocity.length() > 0.0:
