8355ce785f66f8f643c2acdd54dd5cd9f36631f2
ylirin <xili1@s.sfusd.edu>
2024-05-13 14:47:38 -0700

    Spawn Random Mobs

--- player.gd
+++ player.gd
@@ -7,7 +7,7 @@ var health = 100
 
 func _physics_process(delta:):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = direction * 600.0
+	velocity = direction * 2000.0
 	move_and_slide()
 
 	if velocity.length() > 0.0:
