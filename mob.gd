683d3471ab5ef9cb0d03087ad276bd3094dc3c81
ylirin <xili1@s.sfusd.edu>
2024-05-13 14:18:15 -0700

    Add Smoke To Mob

--- mob.gd
+++ mob.gd
@@ -18,3 +18,8 @@ func take_damage():
 	
 	if health == 0:
 		queue_free()
+		
+		const SMOKE_SCENE = preload("res://smoke_explosion/smoke_explosion.tscn")
+		var smoke = SMOKE_SCENE.instantiate()
+		get_parent().add_child(smoke)
+		smoke.global_position = global_position
