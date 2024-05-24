5d76b8fae0c0119f78954c1b3c790e911f4ded43
rxsiebearr <chzou1@s.sfusd.edu>
2024-05-23 18:57:41 -0700

    Revert "Create Label + Counter"
    This reverts commit ff05983f8a1c07c2fcf070e695bfc92f8c50f98b.

--- Label.gd
+++
@@ -1,7 +0,0 @@
-extends Label
-
-var score = 0
-
-func set_score(new_score: int) -> void:
-	score = new_score
-	$Label.text = "x" + str(score)
--- player.gd
+++ player.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 signal health_depleted
 @onready var po: AnimatedSprite2D = $Po
 var health = 150
-	
+
+
 func _physics_process(delta:):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * 1000.0
