c9cffab21904d3cd4142cc998c3f46830da545fc
rxsiebearr <chzou1@s.sfusd.edu>
2024-10-28 00:19:55 -0700

    Revert "pickup for stick"
    This reverts commit 781064f7ed21c1c929e7b6a6c8ae75d4498eff34.

--- survivors_game.gd
+++ survivors_game.gd
@@ -1,20 +1,19 @@
 extends Node2D
 
 @onready var sprite_2d = $Sprite2D
-var my_goal = 2
+var my_goal = 10
 
 func _ready():
 	$BackgroundMusic.play()
-	
-func _process(float) -> void:
-	if Xlbcounter.score >= my_goal:
-		$StickPickupArea.visible = true
-		
+#func _process(float) -> void:
+	#if Xlbcounter.score >= my_goal:
+		#sprite_2d.visible = true
 func spawn_mob():
 	var new_mob = preload("res://mob.tscn").instantiate()
 	%PathFollow2D.progress_ratio = randf()
 	new_mob.global_position = %PathFollow2D.global_position
 	add_child(new_mob)
+
 
 func _on_timer_timeout():
 	spawn_mob()
